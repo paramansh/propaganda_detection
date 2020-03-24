8afabc54131439547b75af30bfb4089021936f94
Paramansh Singh <paramansh9895@gmail.com>
2020-03-25 04:00:15 +0530

    add interactive prediction mode

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import numpy as np
+import argparse
 
 import config
 import input_processing
@@ -8,6 +9,11 @@ import pred_utils
 import utils
 
 from transformers import BertTokenizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--interactive', nargs='?',  type=bool, default=False, help='Set True to enter custom input') 
+
+args = parser.parse_args()
 
 home_dir = "./"
 data_dir = os.path.join(home_dir, "datasets")
@@ -18,9 +24,13 @@ if not os.path.isdir(model_dir):
 tokenizer = config.tokenizer
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+if args.interactive:
+  print("Enter Input: ")
+  input_sentence = input()
+  test_articles = [input_sentence]
+else:
+  test_articles = ["Mini Mike, don’t lick your dirty fingers. Both unsanitary and dangerous to others and yourself!", "Just a random piece of text which should be a normal text"]
 print("Starting prediction")
-test_articles = ["Mini Mike, don’t lick your dirty fingers. Both unsanitary and dangerous to others and yourself!", "Just a random piece of text which should be a normal text"]
-ind = 0
 model = torch.load(os.path.join(model_dir, 'model_370_44_bioe.pt'), map_location={'cuda:0':'cpu'})
 test_spans = [[]] * len(test_articles)
 
